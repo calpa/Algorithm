5da115bc688ff2642d507cc70c903fde40309dfb
Calpa Liu <calpaliu@gmail.com>
2016-10-14 22:55:53 +0800

    Tab -> Spaces

--- sorting/factorial.py
+++ sorting/factorial.py
@@ -7,7 +7,7 @@ __mtime__ = '14/10/2016'
 
 
 def factorial(n):
-	if n == 0:
-		return 1
-	else:
-		return n * factorial(n-1)
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
