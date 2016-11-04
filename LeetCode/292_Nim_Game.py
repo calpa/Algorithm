7993ea24549a165ac7e5cf82b66cd253918a8b4a
Calpa Liu <calpaliu@gmail.com>
2016-11-04 19:01:47 +0800

    Math Question

---
+++ LeetCode/292_Nim_Game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__author__ = 'calpa'
+__mtime__ = '4/11/2016'
+"""
+
+
+def nim(number):
+    # This is a math question...
+    return number % 4 != 0
